8858a4bedcc28a16c95ea529233d6add524130f6
Johnatan Sena <johnatansenna@JohnLinux.John>
2024-05-06 19:10:17 -0300

    create router details movie

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import api from "../../Service"
 import { useParams } from "react-router-dom"
+import './info-filmes.css'
 
 function Filme(){
   const { id } = useParams()
-  const [filme, setFilmes] = useState()
+  const [filmes, setFilmes] = useState({})
   // const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -16,23 +17,34 @@ function Filme(){
         }
       })
       .then((response) => {
-        setFilmes(console.log(response))
+        setFilmes(response.data)
+        console.log(response.data)
       })
       .catch(() => {
         console.log('filme não encontrado')
       })
     }
     detaisFilms()
-    return () => {
-      console.log('Componente Desmontado')
-    }
+  
   }, [])
 
   return (
-    <div>
-      <h1> Veja seus Filmes Favoritos abaixo: </h1>
-      <h1>{console.log(filme)}</h1>
-      <img src={`http://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}></img>
+    <div className="filme-details">
+      <h1 className="text-page"> Veja seus Filmes Favoritos abaixo: </h1>
+      <h1 className="movie-title">{filmes.title}</h1>
+      <img src={`http://image.tmdb.org/t/p/original/${filmes.backdrop_path}`} alt={filmes.id}></img>
+      <h3>Sinapse</h3>
+      <p className="sinapse-text">{filmes.overview}</p>
+      <h3 className="release">Lançamento: {filmes.release_date}</h3>
+      <h3 className="average"> Avaliação: {filmes.vote_average} / 10</h3>
+
+      <div className="area-buttons">
+      <button className="btn-save"> Salvar </button>
+      <button className="btn-trailer">
+        <a href="#"> Trailer </a>
+      </button>
+      </div>
+
     </div>
   )
 }
